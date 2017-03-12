7e41114e4b2b6a0499ca737b7f1c0b5a66355b39
neptunicjenkins <chrisfromtemporaryid@gmail.com>
2017-03-12 18:23:24 -0400

    Log-in Completed Successfully
    We didn't need a URI after all! Bless localhost

--- spot_my_new_song.py
+++ spot_my_new_song.py
@@ -2,8 +2,12 @@
 # pause on project until I'm able to get a callback URI
 
 import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
 import tkinter
+import os
 
+scope = "playlist-read-private user-library-read"
 
 # get the username of a user by extraction from URls
 def extract_username(URl):
@@ -15,23 +19,52 @@ def extract_username(URl):
         return URl
 
 
-# get the user information using i/o
-def get_user_information(sp):
+# get the username information using i/o
+def get_username():
     # get the user information
-    user = None
     username = extract_username(input("Please enter your spotify URl\n"))
-    while(user == None):
+    sp = spotipy.Spotify()
+    
+    while True:
         try:
-            return sp.user(username)
+            # test if this is a valid username
+            sp.user(username)
+            return username
         except spotipy.SpotifyException:
             username = extract_username(input("Invalid username, please enter again\n"))
 
 
+# get client credentials
+def get_spotify_session():
+    username = get_username()
+    
+    # if we have a cached token, we don't need client ID/secret keys.
+    if os.path.isfile(".cache-" + username):
+        token = util.prompt_for_user_token(username, "user-library-read",
+                                           "x", "x", "x")
+        return spotipy.Spotify(auth = token)
+    
+    while True:
+        client_id = input("Please enter your client ID key:\n")
+        client_secret = input("Please enter your client secret key\n")
+        
+        try:
+            token = util.prompt_for_user_token(username, scope, 
+                                               client_id, client_secret,
+                                               "http://localhost:8888/callback")
+            sp = spotipy.Spotify(auth = token)
+            
+            # run this command to check if we logged in successfully.
+            sp.me()
+            
+            return sp
+        except spotipy.oauth2.SpotifyOauthError:
+            print("Invalid key info was entered. Please try again.")
+
+
 def main():
-    sp = spotipy.Spotify()
-    #user = get_user_information(sp)
-    #print(user)
     # testing with the different ways one could enter a spotify track.
+    sp = get_spotify_session()
     track = "spotify:track:5PwCuqzezD4a7mfxMNwk86"
     print(sp.track(track))
     track = sp.track("6ZtJwCyyH3HpRXpL1TI1Cp")
@@ -39,7 +72,11 @@ def main():
     l_tracks = list()
     l_tracks.append("6ZtJwCyyH3HpRXpL1TI1Cp")
     print(sp.recommendations(None, None, l_tracks, 2, None))
-
+    
+    # print user info
+    print(sp.me())
+    print(sp.current_user_playlists())
+    
 
 if __name__ == "__main__":
     main()

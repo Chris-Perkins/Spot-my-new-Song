504c65935b46e951fc1a1b98906665ac1aada41e
neptunicjenkins <chrisfromtemporaryid@gmail.com>
2017-03-11 23:24:28 -0500

    Revert "Minor updates to program"
    This reverts commit 17228a51dd5386e6a960ce934ae47085a95b172a.

--- spot_my_new_song.py
+++ spot_my_new_song.py
@@ -1,36 +1,10 @@
 # Slightly modified spotipy tutorial code. Just making sure this works. :)
 
 import spotipy
-import tkinter
-from spotipy.oauth2 import SpotifyClientCredentials
+sp = spotipy.Spotify()
 
+max = 10
 
-# get the username of a user by extraction from URls
-def extract_username(URl):
-    if len(URl) > 13 and URl[:13] == "spotify:user:":
-        return URl[13:]
-    elif len(URl) > 30 and URl[:30] == "https://open.spotify.com/user/":
-        return URl[30:]
-    else:
-        return URl
-
-
-# get the user information using i/o
-def get_user_information(sp):
-    # get the user information
-    user = None
-    username = extract_username(input("Please enter your spotify URl\n"))
-    while(user == None):
-        try:
-            return sp.user(username)
-        except spotipy.SpotifyException:
-            username = extract_username(input("Invalid username, please enter again\n"))
-
-
-def main():
-    sp = spotipy.Spotify()
-    user = get_user_information(sp)
-
-
-if __name__ == "__main__":
-    main()
+results = sp.search(q='Animal Collective', limit=max)
+for i, t in enumerate(results['tracks']['items']):
+    print(' %2d' % (i + 1), t['name'])

ea2f3f93b0d543e1d74de4db99e715fe4a224fec
neptunicjenkins <chrisfromtemporaryid@gmail.com>
2017-04-22 17:04:44 -0400

    Recommendations are starting to work!

--- recommend_handling.py
+++ recommend_handling.py
@@ -7,6 +7,41 @@ import spotipy
 
 SET_YES = {"yes", "y", "ya", "ye"}
 
+
+# in: spotify session, a dictionary of songs
+# out: a list of the corresponding song URIs
+# to use with recommendations function
+def get_list_song_URIs(spotify_session, dict_songs):
+    list_URIs = list()
+    
+    for song in dict_songs["tracks"]["items"]:
+        list_URIs.append(song["track"]["uri"])
+    
+    # while we have not visited every page, iterate through
+    while dict_songs["tracks"]["next"]:
+        # visit the next page...
+        dict_songs["tracks"] = spotify_session.next(dict_songs["tracks"])
+        
+        # get all items in this page
+        for item in dict_songs["tracks"]["items"]:
+            list_URIs.append(item["track"]["uri"])
+    
+    return list_URIs
+
+# in: spotify_session, list of song URIs
+# out: list of recommendations
+def get_recommendations(spotify_session, list_URIs):
+    list_recommendations = list()
+    
+    # get recommendations based on every list of 5 songs (5 songs is max seed limit
+    # per the Spotify API).
+    for i in range(len(list_URIs) // 5 + (1 if len(list_URIs) % 5 != 0 else 0)):
+        # get the recommendations for these 5 songs
+        list_recommendations.extend(spotify_session.recommendations(seed_tracks = 
+                                     list_URIs[i * 5 : min(i * 5 + 5, len(list_URIs))])["tracks"]
+                                    )
+    print("\n".join(str(x) for x in list_recommendations))
+
 # get recommendations of playlists
 def get_playlist_recommendations(spotify_session):
     
@@ -50,40 +85,31 @@ def get_playlist_recommendations(spotify_session):
     # entry point for this function
     def main():
         # list of songs/albums/artists we'll be getting recommendations from
-        list_songs = list()
+        list_URIs = list()
         
         add_more = True
         while add_more:
             playlist = get_playlist_choice()
             
-            
             results = spotify_session.user_playlist(spotify_session.me()["id"], playlist["id"],
                            fields="tracks,next")
-            tracks = results['tracks']
             
-            for item in tracks['items']:
-                list_songs.append(item['track']['uri'])
-            
-            while tracks['next']:
-                tracks = spotify_session.next(tracks)
-                for item in tracks['items']:
-                    list_songs.append(item['track']['uri'])
+            # get all song URIs from this playlist
+            list_URIs.extend(get_list_song_URIs(spotify_session, results))
             
             print("%s successfully added.\n" % playlist["name"])
             
             add_more = input("Would you like to add another playlist? Y/N\n").lower() in SET_YES
         
-        print(list_songs)
-        print(spotify_session.recommendations(seed_artists = None, seed_albums = None, seed_tracks=list_songs, limit = 20))
-        return list_songs
-    
-    
+            #print(spotify_session.recommendations(seed_artists = None, seed_albums = None, seed_tracks=list_songs, limit = 20))
+            get_recommendations(spotify_session, list_URIs)
+        
     main()
 
 
 # skeleton of later function
 def get_album_recommendations(spotify_session):
-    list_songs = list()
+    list_URIs = list()
     
     add_more = True
     while add_more:
